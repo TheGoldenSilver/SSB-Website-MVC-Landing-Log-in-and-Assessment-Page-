740d21d5c49ff7ebfd5c9bcad56824e9714c7cdd
GoldenSilver <snpgonzales7@gmail.com>
2025-03-20 20:46:03 +0800

    Add project files.

---
+++ SSB Website MVC (Landing, Log-in and Assessment Page)/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SSB_Website_MVC__Landing__Log_in_and_Assessment_Page_.Models;
+
+namespace SSB_Website_MVC__Landing__Log_in_and_Assessment_Page_.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ SSB Website MVC (Landing, Log-in and Assessment Page)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SSB_Website_MVC__Landing__Log_in_and_Assessment_Page_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
