dc1297fd19dcd73e12320a42b8dd3045b005b8b3
TheGoldenSilver <snpgonzales7@gmail.com>
2025-03-21 09:54:34 +0800

    Add files via upload

---
+++ SSB Website MVC (Landing, Log-in and Assessment Page)/Models/AppDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace SSB_Website_MVC__Landing__Log_in_and_Assessment_Page_.Models
+{
+    public class AppDbContext
+    {
+    }
+}
---
+++ SSB Website MVC (Landing, Log-in and Assessment Page)/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkillSandbox.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string username { get; set; }
+
+        [Required]
+        public string password { get; set; }
+    }
+}
